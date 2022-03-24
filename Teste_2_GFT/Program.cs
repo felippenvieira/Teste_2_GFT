050c596fef1021826822dc4fdd0a9b389d8b727e
Felippe <felippe_nvieira@hotmail.com>
2022-03-24 16:34:57 -0300

    Add project files.

---
+++ Teste_2_GFT/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Bem vindo ao programa de música do Elefante incomoda.");
+Console.Write("Escreva um número: ");
+var option = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < option; i++)
+{
+    Console.Write("Incomoda ");
+}
+
+Console.Write("muito mais");
